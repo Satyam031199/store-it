d7bc4da8cdc146862b034cb19e70eba9d7f6c28f
Satyam Chaturvedi <satyamchaturvedi71@gmail.com>
2024-12-01 19:08:17 +0530

    add actions dropdown

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { convertFileSize } from "@/lib/utils";
 import Thumbnail from "./Thumbnail";
 import FormattedDateTime from "./FormattedDateTime";
+import ActionDropdown from "./ActionDropdown";
 
 const Card = ({ file }: { file: Models.Document }) => {
   return (
@@ -17,7 +18,7 @@ const Card = ({ file }: { file: Models.Document }) => {
         />
 
         <div className="flex flex-col items-end justify-between">
-          Actions
+          <ActionDropdown file={file}/>
           <p className="body-1">{convertFileSize(file.size)}</p>
         </div>
       </div>
